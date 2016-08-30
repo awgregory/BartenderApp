0003e1cd550fccf58793bf832fe3fb5ee9b78d29
Adrian Gregory <adrianwgregory2@gmail.com>
2016-08-29 22:22:50 -0400

    Adding existing BartenderApp project files to source control

---
+++ BartenderApp/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BartenderApp.Models;
+
+namespace BartenderApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        
+        public ActionResult Index()
+        {
+            List<Drink> drinks = new List<Drink>{   
+                new Drink { Name = "Rum and Coke", Description = "Rum mostly coke", Price = 6 },
+                new Drink { Name = "Mojito", Description = "Mint and vodka", Price = 9 },
+                new Drink { Name = "Moscow Mule", Description = "Ginger Beer and vodka", Price = 8 }
+        }; // initialized like pg 74 , rendered pg 166 examples
+
+            return View(drinks);
+        }
+
+        public ViewResult Order()
+        {
+            return View();
+        }
+    }
+}
+
---
+++ BartenderApp/Models/Drink.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BartenderApp.Models
+{
+    public class Drink
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+    }
+}
