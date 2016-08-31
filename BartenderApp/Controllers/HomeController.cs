5cdc6b6b6696d4c68319f346243e58df9bf40dd6
Adrian Gregory <adrianwgregory2@gmail.com>
2016-08-31 13:17:24 -0400

    Adding Additional Changes to the code

--- BartenderApp/Controllers/HomeController.cs
+++ BartenderApp/Controllers/HomeController.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.Collections.Generic;
 using System.Web.Mvc;
 using BartenderApp.Models;
 
@@ -9,22 +6,33 @@ namespace BartenderApp.Controllers
 {
     public class HomeController : Controller
     {
-        
+        private static List<Drink> Orders = new List<Drink>();
+
+        private static List<Drink> Drinks
+            = new List<Drink>()
+            {
+                new Drink {Id = 1, Name = "Rum and Coke", Description = "Rum mostly coke", Price = 6},
+                new Drink {Id = 2, Name = "Mojito", Description = "Mint and vodka", Price = 9},
+                new Drink {Id = 3, Name = "Moscow Mule", Description = "Ginger Beer and vodka", Price = 8}
+            };
+
         public ActionResult Index()
         {
-            List<Drink> drinks = new List<Drink>{   
-                new Drink { Name = "Rum and Coke", Description = "Rum mostly coke", Price = 6 },
-                new Drink { Name = "Mojito", Description = "Mint and vodka", Price = 9 },
-                new Drink { Name = "Moscow Mule", Description = "Ginger Beer and vodka", Price = 8 }
-        }; // initialized like pg 74 , rendered pg 166 examples
-
-            return View(drinks);
+            return View(Drinks);
         }
 
-        public ViewResult Order()
+
+        public ActionResult Order(Drink drink)
         {
-            return View();
+            
+            Orders.Add(drink);
+
+            return View(Orders);
+            
         }
+        
+        
     }
 }
-
+// In chrome dev tools for the actionlink I think 1 is an ID assigned by browser
+// for each link it enumerates?   the Home/Order/1 
--- BartenderApp/Models/Drink.cs
+++ BartenderApp/Models/Drink.cs
@@ -7,8 +7,10 @@ namespace BartenderApp.Models
 {
     public class Drink
     {
+        public int Id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
         public decimal Price { get; set; }
     }
 }
+
